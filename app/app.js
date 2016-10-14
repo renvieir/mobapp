1e9d73f113ddaac8d31223dbf36d9e970fbe1b3f
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2016-10-14 08:52:21 -0400

    Adicionando ngRoute para SPA.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
 (function(){
-    angular.module('MobApp',[]);
+    angular.module('MobApp',['ngRoute']).config(function($routeProvider){
+        $routeProvider
+        .when('/', {
+            templateUrl: 'app/templates/directives/mob-call-to-action.html'
+        })
+        .when('/sobre',{
+            templateUrl: 'app/templates/pages/about.html'
+        })
+        .when('/observacoes', {
+            templateUrl: 'app/templates/pages/observacoes.html'
+        })
+        ;
+    });
 })();
